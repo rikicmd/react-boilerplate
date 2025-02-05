7cd86acb150a8f5355444e467287bddb00c5b012
Riki <riki.cmd@gmail.com>
2025-02-05 17:57:23 +0700

    feat: initial unit test using vitest

---
+++ src/tests/setupTests.ts
@@ -0,0 +1,12 @@
+import { server } from "@/mocks/server";
+import { beforeAll, afterEach, afterAll } from "vitest";
+
+// Start the server before all tests
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+
+// Reset any request handlers that are declared as a part of our tests
+// (i.e. for testing one-time error scenarios)
+afterEach(() => server.resetHandlers());
+
+// Clean up after the tests are finished
+afterAll(() => server.close());
