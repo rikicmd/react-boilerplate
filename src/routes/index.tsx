fdd3468e43eef6950fc2b1882d1cbed18fa0f232
Riki <34589356+rikicmd@users.noreply.github.com>
2025-02-06 11:29:21 +0700

    build(react-helmet): setup (#42)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,18 @@
 import { MainPage } from "@/pages";
 import { createFileRoute } from "@tanstack/react-router";
+import { Helmet } from "react-helmet";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
-  return <MainPage />;
+  return (
+    <>
+      <Helmet>
+        <title>My Template React Boilerplate</title>
+      </Helmet>
+      <MainPage />
+    </>
+  );
 }
